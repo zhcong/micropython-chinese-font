37a65cd22584ea8a650241db8dc5978696fcefc7
zhcong <1011.0011@163.com>
2021-02-18 00:06:48 +0800

    抬高变为手动

--- make_db/convert.py
+++ make_db/convert.py
@@ -8,13 +8,6 @@ def print_import(text):
     print(text)
     print('-------------------------')
 
-def auto_cut_func(img, draw, chart, font, size):
-    draw.text((0,0), chart,font = font, fill='black')
-    img_np = np.array(img)
-    index = np.where(img_np > 0)
-    draw.rectangle((0, 0, size, size), fill=(0, 0, 0, 0))
-    return (0 if len(index[0]) == 0 else index[0][0])
-
 def load_chinese(file_name):
     with open(file_name) as fp:
         charts = fp.readline()
@@ -25,31 +18,30 @@ def binarization(img, size):
     img_result_list = bitarray()
     for pix_line in img_np:
         for pix in pix_line:
-            if(pix[3] >= 255/2):
+            if pix[3] >= 255/2:
                 img_result_list.append(True)
             else:
                 img_result_list.append(False)
     return img_result_list.tobytes()
 
-def convert_chart(chart, font, size, auto_cut):
+def convert_chart(chart, font, size, line_uplift):
     img = Image.new('RGBA', (size, size), (0, 0, 0, 0))
     draw = ImageDraw.Draw(img)
-    if(auto_cut):
-        cut_line = auto_cut_func(img, draw, chart, font, size)
-        draw.text((0,-cut_line), chart,font = font, fill='black')
+    if not line_uplift == 0:
+        draw.text((0,0 - line_uplift), chart,font = font, fill='black')
     else:
         draw.text((0,0), chart,font = font, fill='black')
     img_np = binarization(img, size)
     return img_np
 
 if __name__=='__main__':
-    if(len(sys.argv)<4):
-        print_import('参数缺失，python3 convert.py xxx.otf xxx.txt size auto_cut')
+    if len(sys.argv)<4 :
+        print_import('参数缺失，python3 convert.py xxx.otf xxx.txt size line_uplift')
         exit(-1)
     print_import('字形文件%s，筛选字符集文件%s' %(sys.argv[1], sys.argv[2]))
 
     size = int(sys.argv[3])
-    auto_cut = sys.argv[4] == "true"
+    line_uplift = int(sys.argv[4])
 
     charts = load_chinese(sys.argv[2])
     print_import('读取筛选字符集文件\nover')
@@ -68,7 +60,7 @@ if __name__=='__main__':
         char_bin_data_map = {}
         # i从0开始
         for i, chart in enumerate(charts):
-            char_bin_data = convert_chart(chart, font, size, auto_cut)
+            char_bin_data = convert_chart(chart, font, size, line_uplift)
             # [utf8编码长度，占用1byte][utf8编码，占用1或3bype][排序，占用4byte]
             chart_byte = bytes(chart,encoding='utf-8')
             chart_key = len(chart_byte).to_bytes(1, 'big')
